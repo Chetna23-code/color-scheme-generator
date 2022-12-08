19fd57ae2ff02dc47ebc082a414c1fbf6afc2ccf
Chetna23-code <69683597+Chetna23-code@users.noreply.github.com>
2022-12-08 12:21:45 +0530

    Add files via upload

---
+++ color-schme-generator/index.js
@@ -0,0 +1,66 @@
+const colorModeSelect = document.getElementById("color-mode")
+const colorSchemeBtn = document.getElementById("color-btn")
+const colorPicker = document.getElementById("color-picker")
+const mainEl = document.querySelector("main")
+const footerEl = document.querySelector("footer")
+const snackbar = document.getElementById("snackbar")
+const baseUrl = "https://www.thecolorapi.com/scheme"
+let schemes
+
+randomColor = Math.floor(Math.random() * 16777215)
+colorPicker.value = '#' + randomColor.toString(16)
+getColors()
+
+
+colorSchemeBtn.addEventListener("click", () => {
+    getColors()
+})
+
+
+function getColors() {
+    fetch(`${baseUrl}?hex=${colorPicker.value.slice(1)}&${colorModeSelect.value}`)
+        .then(res => res.json())
+        .then(data => {
+            renderColors(data.colors)
+
+            if (!schemes) {
+                schemes = data._links.schemes
+                addSchemeModes()
+            }
+        })
+}
+
+function addSchemeModes() {
+    Object.entries(schemes).forEach(
+        ([name, value]) => {
+            colorModeSelect.add(
+                new Option(name.charAt(0).toUpperCase() + name.slice(1), value)
+            )
+        })
+}
+
+function renderColors(colorsArray) {
+    mainEl.innerHTML = colorsArray.map(color => {
+        return `
+            <div 
+                onclick="copyToClipboard('${color.hex.value}')"
+                class="colors" 
+                style="background-color: ${color.hex.value}">
+                <div class="color-name">${color.name.value}</div>
+            </div>
+        `
+    }).join('')
+
+    footerEl.innerHTML = colorsArray.map(color => {
+        return `
+            <p 
+                onclick="copyToClipboard('${color.hex.value}')"
+                class="color-hex">
+                ${color.hex.value}
+            </p>
+        `
+    }).join('')
+}
+
+  
+  
